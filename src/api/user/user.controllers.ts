61c12467f21a878dcddfde86e436292338fe8491
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:55:01 -0500

    Add user controller for fetching with id

--- src/api/user/user.controllers.ts
+++ src/api/user/user.controllers.ts
@@ -6,6 +6,16 @@ const getUsersController = async (_req: Request, res: Response) => {
   res.json(users);
 };
 
+const getUserByIdController = async (req: Request, res: Response) => {
+  const user = await User.findByPk(req.params['id']);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).end();
+  }
+};
+
 export default {
   getUsersController,
+  getUserByIdController,
 };
