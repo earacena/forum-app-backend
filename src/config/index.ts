64d720dfc610c0712503f361a04d2749f53d0d16
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 01:19:54 -0500

    Refactor environment variable processing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,28 +1,32 @@
 import 'dotenv/config';
 
-const validateEnvironmentVariable = (value: string | undefined): string => {
-  return value ? value : '';
-};
+const validateEnvironmentVariable = (value: string | undefined): string => value || '';
 
-const isValidNumber = (value: unknown): value is Number => {
+const isValidNumber = (value: unknown): value is number => {
   if (value === undefined || value === null) {
     return false;
   }
 
-  if (isNaN(Number(value))) {
+  if (Number.isNaN(Number(value))) {
     return false;
   }
 
   return true;
 };
 
-export const NODE_ENV = process.env['NODE_ENV'];
+const determineDatabaseUrl = (nodeEnv: string): string => {
+  switch (nodeEnv) {
+    case 'development':
+      return validateEnvironmentVariable(process.env['DEV_DATABASE_URL']);
+    default:
+      return '';
+  }
+};
 
-let DATABASE_URL: string = '';
-if (NODE_ENV === 'development') {
-  DATABASE_URL = validateEnvironmentVariable(process.env['DEV_DATABASE_URL']);
-}
+export const NODE_ENV = validateEnvironmentVariable(process.env['NODE_ENV']);
 
-export const PORT = isValidNumber(validateEnvironmentVariable(process.env['PORT']));
+export const DATABASE_URL = determineDatabaseUrl(NODE_ENV);
 
-export { DATABASE_URL };
+export const PORT = isValidNumber(
+  validateEnvironmentVariable(process.env['PORT']),
+);
