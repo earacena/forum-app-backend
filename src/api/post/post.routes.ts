e99eb322a77346171c206f8420452a7be955332c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:45:34 -0500

    Add post controller with id fetching to routes

--- src/api/post/post.routes.ts
+++ src/api/post/post.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import postControllers from './post.controllers';
 
-const { getPostsController } = postControllers;
+const { getPostsController, getPostByIdController } = postControllers;
 
 const postsRouter = express.Router();
 
 postsRouter.get('/', getPostsController);
+postsRouter.get('/:id', getPostByIdController);
 
 export default postsRouter;
