97174d762de947a3febf6b318fb96634a4338df5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 01:27:30 -0500

    Refactor app initialization

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import usersRouter from './api/user/user.routes';
 import postsRouter from './api/post/post.routes';
 import threadsRouter from './api/thread/thread.routes';
+import { PORT } from './config';
+import { connectToDatabase } from './utils/db';
 
 const app = express();
 
@@ -12,5 +14,11 @@ app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/threads', threadsRouter);
 
+const main = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server @ port ${PORT}`);
+  });
+};
 
-export default app;
+export default { main };
