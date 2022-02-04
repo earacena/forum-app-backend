fe043c028b8f53674eaa693612af806d2a57b2ad
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 03:09:25 -0500

    Refactor user controller to fetch from database

--- src/api/user/user.controllers.ts
+++ src/api/user/user.controllers.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
+import User from './user.model';
 
-export const getUsersController = async (_req: Request, res: Response) => {
-  res.send('Getting all users...');
+const getUsersController = async (_req: Request, res: Response) => {
+  const users = await User.findAll();
+  res.json(users);
+};
+
+export default {
+  getUsersController,
 };
