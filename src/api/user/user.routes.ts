cdbba5191c2740ab73b4fb62262f1b081c35b4cf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:55:39 -0500

    Add user controller with id fetching to routes

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import userControllers from './user.controllers';
 
-const { getUsersController } = userControllers;
+const { getUsersController, getUserByIdController } = userControllers;
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', getUsersController);
+usersRouter.get('/:id', getUserByIdController);
 
 export default usersRouter;
