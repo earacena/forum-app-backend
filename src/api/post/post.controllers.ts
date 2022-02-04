a34a4fdc9de1fa6157ec8970a891a83012e06261
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:44:06 -0500

    Add post controller for fetching with id

--- src/api/post/post.controllers.ts
+++ src/api/post/post.controllers.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express';
 import Post from './post.model';
 
-const getPostsController = async (_req: Request, res: Response): Promise<Response> => {
+const getPostsController = async (_req: Request, res: Response) => {
   const posts = await Post.findAll();
-  return res.status(200).json(posts);
+  res.status(200).json(posts);
+};
+
+const getPostByIdController = async (req: Request, res: Response) => {
+  const post = await Post.findByPk(req.params['id']);
+  if (post) {
+    res.status(200).json(post);
+  } else {
+    res.status(404).end();
+  }
 };
 
 export default {
   getPostsController,
+  getPostByIdController,
 };
