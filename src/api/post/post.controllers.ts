61923e2c713b9c4ba180e7f0bcb6c40533deb767
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 03:07:05 -0500

    Refactor post controller to fetch from database

--- src/api/post/post.controllers.ts
+++ src/api/post/post.controllers.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
+import Post from './post.model';
 
-export const getPostsController = async (_req: Request, res: Response) => {
-  res.send('Getting all posts...');
+const getPostsController = async (_req: Request, res: Response): Promise<Response> => {
+  const posts = await Post.findAll();
+  return res.status(200).json(posts);
+};
+
+export default {
+  getPostsController,
 };
