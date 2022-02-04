b6c8a507e3460ef9f9069de36db43457037a65bc
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:49:16 -0500

    Add thread controller for fetching with id

--- src/api/thread/thread.controllers.ts
+++ src/api/thread/thread.controllers.ts
@@ -6,6 +6,16 @@ const getThreadsController = async (_req: Request, res: Response) => {
   res.json(threads);
 };
 
+const getThreadByIdController = async (req: Request, res: Response) => {
+  const thread = await Thread.findByPk(req.params['id']);
+  if (thread) {
+    res.status(200).json(thread);
+  } else {
+    res.status(404).end();
+  }
+};
+
 export default {
   getThreadsController,
+  getThreadByIdController,
 };
