0d94e60dc9a2395201d979d1bbb81a85f6302063
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-04 18:50:14 -0500

    Add thread controller with id fetching to routes

--- src/api/thread/thread.routes.ts
+++ src/api/thread/thread.routes.ts
@@ -3,10 +3,12 @@ import threadControllers from './thread.controllers';
 
 const {
   getThreadsController,
+  getThreadByIdController,
 } = threadControllers;
 
 const threadsRouter = express.Router();
 
 threadsRouter.get('/', getThreadsController);
+threadsRouter.get('/:id', getThreadByIdController);
 
 export default threadsRouter;
