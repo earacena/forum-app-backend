6a19d153ee59e8ac9a90b2a62aa5642b3cef9ec2
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-06 15:16:18 -0500

    Refactor post tests to clear type warnings

--- src/api/post/post.test.ts
+++ src/api/post/post.test.ts
@@ -8,15 +8,13 @@ describe('Forum API', () => {
   describe('when retrieving Posts', () => {
     test('successfully retrieves all posts', async () => {
       const response = await api.get('/api/posts').expect(200);
-      const posts = JSON.parse(response.text);
-      expect(posts).toHaveLength(1);
+      expect(JSON.parse(response.text)).toHaveLength(1);
     });
 
     test('successfully a post by id', async () => {
       const response = await api.get('/api/posts/1').expect(200);
-      const post = JSON.parse(response.text);
-      expect(post).toBeDefined();
-      expect(post.content).toBe('This is my first post in this thread');
+      expect(JSON.parse(response.text)).toBeDefined();
+      expect(JSON.parse(response.text).content).toBe('This is my first post in this thread');
     });
   });
 
