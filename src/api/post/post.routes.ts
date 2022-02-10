92ecdd3f6daa9e018bbc3e764e004863f48c141d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-09 20:51:12 -0500

    Add route for creating posts

--- src/api/post/post.routes.ts
+++ src/api/post/post.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import postControllers from './post.controllers';
 
-const { getPostsController, getPostByIdController } = postControllers;
+const { getPostsController, getPostByIdController, deletePostByIdController, createPostController } = postControllers;
 
 const postsRouter = express.Router();
 
 postsRouter.get('/', getPostsController);
+postsRouter.post('/', createPostController);
 postsRouter.get('/:id', getPostByIdController);
+postsRouter.delete('/:id', deletePostByIdController);
 
 export default postsRouter;
