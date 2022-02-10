eb38a2948064a3e470689337c7ade369e5b1d44c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-09 20:56:21 -0500

    Add route for creating threads

--- src/api/thread/thread.routes.ts
+++ src/api/thread/thread.routes.ts
@@ -4,11 +4,13 @@ import threadControllers from './thread.controllers';
 const {
   getThreadsController,
   getThreadByIdController,
+  createThreadController,
 } = threadControllers;
 
 const threadsRouter = express.Router();
 
 threadsRouter.get('/', getThreadsController);
+threadsRouter.post('/', createThreadController);
 threadsRouter.get('/:id', getThreadByIdController);
 
 export default threadsRouter;
