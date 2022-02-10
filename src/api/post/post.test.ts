ef55e674a4547e2a9d463f768bedb49650702017
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-09 22:47:46 -0500

    Add test for updating posts

--- src/api/post/post.test.ts
+++ src/api/post/post.test.ts
@@ -90,13 +90,52 @@ describe('Post API', () => {
         content: 'Interesting...',
       };
 
-      const response = await api
-        .post('/api/posts')
-        .send(newPost)
-        .expect(201);
+      const response = await api.post('/api/posts').send(newPost).expect(201);
 
       expect(JSON.parse(response.text)).toBeDefined();
       expect(JSON.parse(response.text).content).toBe('Interesting...');
+    });
+  });
+
+  describe('when updating posts', () => {
+    beforeEach(async () => {
+      await Post.sync({ force: true });
+      await Post.create({
+        threadId: 1,
+        userId: 1,
+        content: 'This is what I wanted to discuss.',
+      });
+      await Post.create({
+        threadId: 1,
+        userId: 2,
+        content: 'Very interesting discussion.',
+      });
+      await Post.create({
+        threadId: 2,
+        userId: 2,
+        content: 'Following up on our previous discussion...',
+      });
+    });
+
+    test('successfully updates post by id', async () => {
+      const postToUpdate = {
+        content: 'This is a very interesting discussion.',
+      };
+
+      let response = await api
+        .put('/api/posts/2')
+        .send(postToUpdate)
+        .expect(200);
+
+      expect(JSON.parse(response.text).content).toBe(
+        'This is a very interesting discussion.',
+      );
+
+      response = await api.get('/api/posts/2').expect(200);
+
+      expect(JSON.parse(response.text).content).toBe(
+        'This is a very interesting discussion.',
+      );
     });
   });
 
