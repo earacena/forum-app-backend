e61dbd619cc0491480930aeab6968830cdc5efa2
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-10 17:24:27 -0500

    Update user tests to include password hashing

--- src/api/user/user.test.ts
+++ src/api/user/user.test.ts
@@ -1,7 +1,10 @@
 import supertest from 'supertest';
+import bcrypt from 'bcrypt';
+import { String as RtString } from 'runtypes';
 import app from '../../app';
 import { sequelize } from '../../utils/db';
 import User from './user.model';
+import { User as UserType, UserArray as UserArrayType } from './user.types';
 
 const api = supertest(app.app);
 
@@ -12,22 +15,27 @@ describe('User API', () => {
       await User.create({
         username: 'user1',
         name: 'User One',
+        passwordHash: RtString.check(await bcrypt.hash('password1', 10)),
       });
+
       await User.create({
         username: 'user2',
         name: 'User Two',
+        passwordHash: RtString.check(await bcrypt.hash('password2', 10)),
       });
     });
 
     test('successfully retrieves all users', async () => {
       const response = await api.get('/api/users').expect(200);
-      expect(JSON.parse(response.text)).toHaveLength(2);
+      const users = UserArrayType.check(JSON.parse(response.text));
+      expect(users).toHaveLength(2);
     });
 
     test('successfully a user by id', async () => {
       const response = await api.get('/api/users/2').expect(200);
-      expect(JSON.parse(response.text)).toBeDefined();
-      expect(JSON.parse(response.text).username).toBe('user2');
+      const user = UserType.check(JSON.parse(response.text));
+      expect(user).toBeDefined();
+      expect(user.username).toBe('user2');
     });
   });
 
@@ -37,34 +45,38 @@ describe('User API', () => {
       await User.create({
         username: 'user1',
         name: 'User One',
+        passwordHash: RtString.check(await bcrypt.hash('password1', 10)),
       });
 
       await User.create({
         username: 'user2',
         name: 'User Two',
+        passwordHash: RtString.check(await bcrypt.hash('password2', 10)),
       });
 
       await User.create({
         username: 'user3',
         name: 'User Three',
+        passwordHash: RtString.check(await bcrypt.hash('password3', 10)),
       });
     });
 
     test('successfully deletes user', async () => {
       const response = await api.get('/api/users/').expect(200);
-      await api
-        .delete(`/api/users/${JSON.parse(response.text)[0].id}`)
-        .expect(204);
+      const users = UserArrayType.check(JSON.parse(response.text));
+      const user = UserType.check(users[0]);
+      await api.delete(`/api/users/${user.id}`).expect(204);
     });
 
     test('deletion is reflected in length of returned users', async () => {
       let response = await api.get('/api/users/').expect(200);
-      await api
-        .delete(`/api/users/${JSON.parse(response.text)[0].id}`)
-        .expect(204);
+      let users = UserArrayType.check(JSON.parse(response.text));
+      const user = UserType.check(users[0]);
+      await api.delete(`/api/users/${user.id}`).expect(204);
 
       response = await api.get('/api/users').expect(200);
-      expect(JSON.parse(response.text)).toHaveLength(2);
+      users = UserArrayType.check(JSON.parse(response.text));
+      expect(users).toHaveLength(2);
     });
   });
 
@@ -77,14 +89,17 @@ describe('User API', () => {
       const newUser = {
         name: 'New User',
         username: 'newuser',
+        password: 'secretkey',
       };
 
       const response = await api.post('/api/users').send(newUser).expect(201);
+      const user = UserType.check(JSON.parse(response.text));
 
-      expect(JSON.parse(response.text)).toBeDefined();
-      expect(JSON.parse(response.text).username).toBe('newuser');
-      expect(JSON.parse(response.text).name).toBe('New User');
-      expect(JSON.parse(response.text).id).toBe(1);
+      expect(user).toBeDefined();
+      expect(user.username).toBe('newuser');
+      expect(user.name).toBe('New User');
+      expect(user.id).toBe(1);
+      expect(user.passwordHash).toBeDefined();
     });
   });
 
