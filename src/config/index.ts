93783e99f644cb8d9b7f3c7d74b7c670ecf720ae
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-10 20:02:01 -0500

    Refactor env variable processing to use runtypes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,36 +1,18 @@
 import 'dotenv/config';
-
-const validateEnvironmentVariable = (value: string | undefined): string => value || '';
-
-const isValidNumber = (value: unknown): value is number => {
-  if (value === undefined || value === null) {
-    return false;
-  }
-
-  if (Number.isNaN(Number(value))) {
-    return false;
-  }
-
-  return true;
-};
+import { String as RtString, Number as RtNumber } from 'runtypes'
 
 const determineDatabaseUrl = (nodeEnv: string): string => {
   switch (nodeEnv) {
     case 'development':
-      return validateEnvironmentVariable(process.env['DEV_DATABASE_URL']);
+      return RtString.check(process.env['DEV_DATABASE_URL']);
     case 'testing':
-      return validateEnvironmentVariable(process.env['TEST_DATABASE_URL']);
+      return RtString.check(process.env['TEST_DATABASE_URL']);
     default:
       return '';
   }
 };
 
-export const NODE_ENV = validateEnvironmentVariable(process.env['NODE_ENV']);
-
+export const NODE_ENV = RtString.check(process.env['NODE_ENV']);
 export const DATABASE_URL = determineDatabaseUrl(NODE_ENV);
 
-export const PORT: number = isValidNumber(
-  validateEnvironmentVariable(process.env['PORT']),
-)
-  ? Number(process.env['PORT'])
-  : 3001;
+export const PORT: number = RtNumber.check(process.env['PORT']);
