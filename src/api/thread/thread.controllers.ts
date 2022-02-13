fd216c71711bf7ca5849f5428d95621bab3e0022
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-12 20:35:00 -0500

    Add token authentication and error handling

--- src/api/thread/thread.controllers.ts
+++ src/api/thread/thread.controllers.ts
@@ -1,38 +1,121 @@
 import { Request, Response } from 'express';
-import { ThreadPostRequest } from './thread.types';
+import {
+  RequestIdParam,
+  RtValidationError,
+  Thread as ThreadType,
+  ThreadArray as ThreadArrayType,
+  ThreadPostRequest,
+  ThreadDeleteRequest,
+  decodedToken as decodedTokenType,
+} from './thread.types';
+
+import { User as UserType } from '../user/user.types';
+import User from '../user/user.model';
 import Thread from './thread.model';
 
 const getThreadsController = async (_req: Request, res: Response) => {
-  const threads = await Thread.findAll();
-  res.json(threads);
-};
+  try {
+    const threads = ThreadArrayType.check(await Thread.findAll());
+    res.json(threads);
+  } catch (error) {
+    // console.error(error);
+    if (RtValidationError.guard(error)) {
+      if (error.code === 'CONTENT_INCORRECT' && error.details) {
+        if ('decodedToken' in error.details) {
+          res.status(401).json({ error: 'invalid or missing token' }).end();
+        }
 
-const createThreadController = async (req: Request, res: Response) => {
-  const { title, userId } = ThreadPostRequest.check(req.body);
-  const newThread = await Thread.create({
-    title,
-    userId,
-  });
-  res.status(201).json(newThread);
+        res.status(400).json({ error: error.details }).end();
+      }
+    }
+  }
 };
 
 const getThreadByIdController = async (req: Request, res: Response) => {
-  const thread = await Thread.findByPk(req.params['id']);
-  if (thread) {
+  try {
+    const { id } = RequestIdParam.check({ id: req.params['id'] });
+    const thread = ThreadType.check(await Thread.findByPk(id));
     res.status(200).json(thread);
-  } else {
-    res.status(404).end();
+  } catch (error) {
+    // console.error(error);
+    if (RtValidationError.guard(error)) {
+      if (error.code === 'CONTENT_INCORRECT' && error.details) {
+        if ('decodedToken' in error.details) {
+          res.status(401).json({ error: 'invalid or missing token' }).end();
+        }
+
+        res.status(400).json({ error: error.details }).end();
+      }
+    }
+  }
+};
+
+const createThreadController = async (req: Request, res: Response) => {
+  try {
+    const { title, decodedToken } = ThreadPostRequest.check(req.body);
+
+    const token = decodedTokenType.check(decodedToken);
+    const user = UserType.check(await User.findByPk(token.id));
+
+    if (!user) {
+      res.status(400).json({ error: 'user does not exist' });
+    }
+
+    const newThread = await Thread.create({
+      userId: user.id,
+      title,
+    });
+
+    res.status(201).json(newThread).end();
+  } catch (error) {
+    // console.error(error);
+    if (RtValidationError.guard(error)) {
+      if (error.code === 'CONTENT_INCORRECT' && error.details) {
+        if ('decodedToken' in error.details) {
+          res.status(401).json({ error: 'invalid or missing token' }).end();
+        }
+
+        res.status(400).json({ error: error.details }).end();
+      }
+    }
   }
 };
 
 const deleteThreadByIdController = async (req: Request, res: Response) => {
-  await Thread.destroy({
-    where: {
-      id: req.params['id'],
-    },
-  });
+  try {
+    const { decodedToken } = ThreadDeleteRequest.check(req.body);
+    const { id } = RequestIdParam.check({ id: req.params['id'] });
+    const token = decodedTokenType.check(decodedToken);
+    const user = UserType.check(await User.findByPk(token.id));
+    const thread = ThreadType.check(await Thread.findByPk(id));
 
-  res.status(204).end();
+    // console.log(`token: ${token}, postId: ${id}, user: ${user}, post: ${post}`);
+
+    if (user.id !== thread.userId) {
+      res
+        .status(401)
+        .json({ error: "cannot delete another user's thread" })
+        .end();
+    }
+
+    await Thread.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(204).end();
+  } catch (error: unknown) {
+    // console.error(error);
+    if (RtValidationError.guard(error)) {
+      if (error.code === 'CONTENT_INCORRECT' && error.details) {
+        if ('decodedToken' in error.details) {
+          res.status(401).json({ error: 'invalid or missing token' }).end();
+        }
+
+        res.status(400).json({ error: error.details }).end();
+      }
+    }
+  }
 };
 
 export default {
