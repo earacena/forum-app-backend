f7cf240adba70a165e649d51b6ce8c04e90a7eaa
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-12 19:02:05 -0500

    Fix runtypes definition mismatch

--- src/api/thread/thread.types.ts
+++ src/api/thread/thread.types.ts
@@ -5,13 +5,25 @@ import {
   Static as RtStatic,
   Array as RtArray,
   InstanceOf as RtInstanceOf,
+  Union as RtUnion,
+  ValidationError,
 } from 'runtypes';
 
 export const Thread = RtRecord({
   id: RtNumber,
   userId: RtNumber,
   title: RtString,
-  dateCreated: RtInstanceOf(Date),
+  dateCreated: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => {
+      // Must be parsable into  date string
+      if (!x || x === null || typeof x !== 'string' || Number.isNaN(Date.parse(x))) {
+        return false;
+      }
+
+      return true;
+    }),
+  ),
 });
 
 export const ThreadArray = RtArray(Thread);
@@ -22,3 +34,5 @@ export const ThreadPostRequest = RtRecord({
   userId: RtNumber,
   title: RtString,
 });
+
+export const RtValidationError = RtInstanceOf(ValidationError);
