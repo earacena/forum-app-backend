17b9affaacda8c901100a11600e7f8e790e57f36
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-17 14:35:46 -0500

    Change post tests from integration to unit tests

--- src/api/post/post.test.ts
+++ src/api/post/post.test.ts
@@ -1,33 +1,86 @@
 import supertest from 'supertest';
+import { Request, Response, NextFunction } from 'express';
 import app from '../../app';
-import { sequelize } from '../../utils/db';
-import { TokenResponse } from '../login/login.types';
+import 'sequelize';
 import Post from './post.model';
+import User from '../user/user.model';
 import { Post as PostType, PostArray as PostArrayType } from './post.types';
 
 const api = supertest(app.app);
 
-describe('Post API', () => {
-  describe('when retrieving Posts', () => {
-    beforeEach(async () => {
-      await Post.sync({ force: true });
-      await Post.create({
-        threadId: 1,
-        userId: 1,
-        content: 'This is what I wanted to discuss.',
-      });
-      await Post.create({
-        threadId: 1,
-        userId: 2,
-        content: 'Very interesting discussion.',
-      });
-      await Post.create({
-        threadId: 2,
-        userId: 2,
-        content: 'Following up on our previous discussion...',
-      });
-    });
+jest.mock('sequelize');
+jest.mock('../user/user.model');
+jest.mock('./post.model');
+jest.mock(
+  '../../middleware/authenticate',
+  () => (req: Request, _res: Response, next: NextFunction) => {
+    req.body.decodedToken = {
+      id: 1,
+      username: 'mockuser1',
+    };
 
+    next();
+  },
+);
+
+describe('Post API', () => {
+  const mockedPosts = [
+    {
+      id: 1,
+      threadId: 1,
+      userId: 1,
+      content: 'This is what I wanted to discuss.',
+      datePosted: new Date(Date.now()).toDateString(),
+    },
+    {
+      id: 2,
+      threadId: 1,
+      userId: 2,
+      content: 'Very interesting discussion.',
+      datePosted: new Date(Date.now()).toDateString(),
+    },
+    {
+      id: 3,
+      threadId: 2,
+      userId: 2,
+      content: 'Following up on our previous discussion...',
+      datePosted: new Date(Date.now()).toDateString(),
+    },
+  ];
+
+  beforeAll(() => {
+    (User.findByPk as jest.Mock).mockResolvedValue({
+      id: 1,
+      name: 'Mock User 1',
+      username: 'mockuser1',
+      passwordHash: 'password_hash',
+      dateRegistered: new Date(Date.now()).toDateString(),
+    });
+    (Post.findByPk as jest.Mock).mockResolvedValue(mockedPosts[0]);
+    (Post.findAll as jest.Mock).mockResolvedValue(mockedPosts);
+    (Post.create as jest.Mock).mockResolvedValue({
+      id: 4,
+      userId: 1,
+      threadId: 1,
+      content: 'Interesting...',
+      datePosted: new Date(Date.now()).toDateString(),
+    });
+    (Post.update as jest.Mock).mockResolvedValue([
+      1,
+      [
+        {
+          id: 1,
+          threadId: 1,
+          userId: 1,
+          content: 'This is a very interesting discussion.',
+          datePosted: new Date(Date.now()).toDateString(),
+        },
+      ],
+    ]);
+    (Post.destroy as jest.Mock).mockImplementation(() => {});
+  });
+
+  describe('when retrieving Posts', () => {
     test('successfully retrieves all posts', async () => {
       const response = await api.get('/api/posts').expect(200);
       const posts = PostArrayType.check(JSON.parse(response.text));
@@ -35,48 +88,22 @@ describe('Post API', () => {
     });
 
     test('successfully a post by id', async () => {
-      const response = await api.get('/api/posts/2').expect(200);
+      const response = await api.get('/api/posts/1').expect(200);
       const post = PostType.check(JSON.parse(response.text));
       expect(post).toBeDefined();
-      expect(post.content).toBe('Very interesting discussion.');
+      expect(post.content).toBe('This is what I wanted to discuss.');
     });
   });
 
   describe('when deleting posts', () => {
-    beforeEach(async () => {
-      await Post.sync({ force: true });
-      await Post.create({
-        threadId: 1,
-        userId: 1,
-        content: 'This is what I wanted to discuss.',
-      });
-      await Post.create({
-        threadId: 1,
-        userId: 2,
-        content: 'Very interesting discussion.',
-      });
-      await Post.create({
-        threadId: 2,
-        userId: 2,
-        content: 'Following up on our previous discussion...',
-      });
-    });
-
     test('successfully deletes post', async () => {
-      let response = await api.get('/api/posts/').expect(200);
+      const response = await api.get('/api/posts/').expect(200);
       const posts = PostArrayType.check(JSON.parse(response.text));
       const post = PostType.check(posts[0]);
 
-      response = await api
-        .post('/api/login')
-        .send({ username: 'testuser', password: 'testpassword' })
-        .expect(200);
-
-      const { token } = TokenResponse.check(JSON.parse(response.text));
-
       await api
         .delete(`/api/posts/${post.id}`)
-        .set('Authorization', `bearer ${token}`)
+        .set('Authorization', 'bearer token')
         .expect(204);
     });
 
@@ -85,17 +112,12 @@ describe('Post API', () => {
       let posts = PostArrayType.check(JSON.parse(response.text));
       const post = PostType.check(posts[0]);
 
-      response = await api
-        .post('/api/login')
-        .send({ username: 'testuser', password: 'testpassword' })
-        .expect(200);
-
-      const { token } = TokenResponse.check(JSON.parse(response.text));
-
       await api
         .delete(`/api/posts/${post.id}`)
-        .set('Authorization', `bearer ${token}`)
+        .set('Authorization', 'bearer token')
         .expect(204);
+
+      (Post.findAll as jest.Mock).mockResolvedValueOnce(mockedPosts.slice(1));
 
       response = await api.get('/api/posts').expect(200);
       posts = PostArrayType.check(JSON.parse(response.text));
@@ -103,47 +125,36 @@ describe('Post API', () => {
     });
 
     test("returns error when trying to delete other users' post", async () => {
-      let response = await api.get('/api/posts/').expect(200);
+      const response = await api.get('/api/posts/').expect(200);
       const posts = PostArrayType.check(JSON.parse(response.text));
       const post = PostType.check(posts[2]);
 
-      response = await api
-        .post('/api/login')
-        .send({ username: 'testuser', password: 'testpassword' })
-        .expect(200);
-
-      const { token } = TokenResponse.check(JSON.parse(response.text));
+      (Post.findByPk as jest.Mock).mockResolvedValueOnce({
+        id: 3,
+        threadId: 2,
+        userId: 2,
+        content: 'Following up on our previous discussion...',
+        datePosted: 'Thu Feb 17 2022',
+      });
 
       await api
         .delete(`/api/posts/${post.id}`)
-        .set('Authorization', `bearer ${token}`)
+        .set('Authorization', 'bearer token')
         .expect(401);
     });
   });
 
   describe('when creating posts', () => {
-    beforeEach(async () => {
-      await Post.sync({ force: true });
-    });
-
     test('successfully creates post', async () => {
-      let response = await api
-        .post('/api/login')
-        .send({ username: 'testuser', password: 'testpassword' })
-        .expect(200);
-
-      const { token } = TokenResponse.check(JSON.parse(response.text));
-
       const newPost = {
         threadId: 1,
-        userId: 1,
         content: 'Interesting...',
       };
 
-      response = await api
+      const response = await api
         .post('/api/posts')
         .send(newPost)
-        .set('Authorization', `bearer ${token}`)
+        .set('Authorization', 'bearer token')
         .expect(201);
 
       const post = PostType.check(JSON.parse(response.text));
@@ -153,35 +164,34 @@ describe('Post API', () => {
   });
 
   describe('when updating posts', () => {
-    beforeEach(async () => {
-      await Post.sync({ force: true });
-      await Post.create({
-        threadId: 1,
-        userId: 1,
-        content: 'This is what I wanted to discuss.',
-      });
-    });
-
     test('successfully updates post by id', async () => {
       const postToUpdate = {
         content: 'This is a very interesting discussion.',
       };
+      (Post.findByPk as jest.Mock).mockResolvedValueOnce({
+        id: 1,
+        threadId: 1,
+        userId: 1,
+        content: 'This is a very interesting discussion.',
+        datePosted: new Date(Date.now()).toDateString(),
+      });
 
       let response = await api
-        .post('/api/login')
-        .send({ username: 'testuser', password: 'testpassword' })
-        .expect(200);
-
-      const { token } = TokenResponse.check(JSON.parse(response.text));
-
-      response = await api
         .put('/api/posts/1')
         .send(postToUpdate)
-        .set('Authorization', `bearer ${token}`)
+        .set('Authorization', 'bearer token')
         .expect(200);
 
       let post = PostType.check(JSON.parse(response.text));
       expect(post.content).toBe('This is a very interesting discussion.');
+
+      (Post.findByPk as jest.Mock).mockResolvedValueOnce({
+        id: 1,
+        threadId: 1,
+        userId: 1,
+        content: 'This is a very interesting discussion.',
+        datePosted: new Date(Date.now()).toDateString(),
+      });
 
       response = await api.get('/api/posts/1').expect(200);
       post = PostType.check(JSON.parse(response.text));
@@ -190,9 +200,5 @@ describe('Post API', () => {
         'This is a very interesting discussion.',
       );
     });
-  });
-
-  afterAll(async () => {
-    await sequelize.close();
   });
 });
