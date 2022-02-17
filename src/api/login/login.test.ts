24341c451c4b5104052af0b732c4526ec529daf5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-17 14:31:26 -0500

    Change login tests from integration to unit tests

--- src/api/login/login.test.ts
+++ src/api/login/login.test.ts
@@ -1,33 +1,38 @@
 import supertest from 'supertest';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import app from '../../app';
-import { sequelize } from '../../utils/db';
 import User from '../user/user.model';
 import {
-  PasswordHash as PasswordHashType,
   TokenResponse as TokenResponseType,
 } from './login.types';
 
 const api = supertest(app.app);
+jest.mock('jsonwebtoken');
+jest.mock('sequelize');
+jest.mock('../user/user.model');
+jest.mock('bcrypt');
 
 describe('Login API', () => {
-  describe('when receiving a login request', () => {
-    beforeEach(async () => {
-      await User.sync({ force: true });
-      await User.create({
-        username: 'user1',
-        name: 'User One',
-        passwordHash: PasswordHashType.check(
-          await bcrypt.hash('password1', 10),
-        ),
-      });
+  beforeAll(() => {
+    (User.findOne as jest.Mock).mockResolvedValue({
+      id: 1,
+      name: 'Mock User 1',
+      username: 'mockuser1',
+      passwordHash: 'password_hash',
+      dateRegistered: new Date(Date.now()).toDateString(),
     });
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+    (jwt.sign as jest.Mock).mockReturnValue('mock token');
+  });
 
+  describe('when receiving a login request', () => {
     test('successfully returns a token for correct credentials', async () => {
       const response = await api
         .post('/api/login')
-        .send({ username: 'user1', password: 'password1' })
+        .send({ username: 'mockuser1', password: 'password1' })
         .expect(200);
+
       const tokenResponse = TokenResponseType.check(JSON.parse(response.text));
       expect(tokenResponse.token).toBeDefined();
       expect(tokenResponse.username).toBeDefined();
@@ -35,14 +40,18 @@ describe('Login API', () => {
     });
 
     test('returns error for incorrect credentials', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValueOnce(false);
       await api
         .post('/api/login')
-        .send({ username: 'user1', password: 'password2' })
+        .send({ username: 'mockuser1', password: 'password2' })
+        .expect(401);
+
+      (User.findOne as jest.Mock).mockResolvedValueOnce(null);
+
+      await api
+        .post('/api/login')
+        .send({ username: 'user2', password: 'password1' })
         .expect(401);
     });
-  });
-
-  afterAll(async () => {
-    await sequelize.close();
   });
 });
