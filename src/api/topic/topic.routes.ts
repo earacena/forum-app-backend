13856ca8d92b587af4103d115dc08e7467542399
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-28 17:08:44 -0500

    Add new thread fetching controller to topic routes

--- src/api/topic/topic.routes.ts
+++ src/api/topic/topic.routes.ts
@@ -4,11 +4,13 @@ import topicControllers from './topic.controllers';
 const {
   getTopicsController,
   getTopicByIdController,
+  getThreadsOfTopicController,
 } = topicControllers;
 
 const topicsRouter = express.Router();
 
 topicsRouter.get('/', getTopicsController);
 topicsRouter.get('/:id', getTopicByIdController);
+topicsRouter.get('/:id/threads', getThreadsOfTopicController);
 
 export default topicsRouter;
