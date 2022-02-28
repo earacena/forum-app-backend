c835e7744ac61980cfe9aa5986cead542fff6b47
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-28 17:08:04 -0500

    Implement controller for fetching threads of topic

--- src/api/topic/topic.controllers.ts
+++ src/api/topic/topic.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import Thread from '../thread/thread.model';
+import { ThreadArray } from '../thread/thread.types';
 import Topic from './topic.model';
 import {
   RtValidationError,
@@ -40,7 +42,29 @@ const getTopicByIdController = async (req: Request, res: Response) => {
   }
 };
 
+const getThreadsOfTopicController = async (req: Request, res: Response) => {
+  try {
+    const { id } = RequestIdParam.check({ id: req.params['id'] });
+    const threads = ThreadArray.check(
+      await Thread.findAll({ raw: true, where: { topicId: id }, order: [['id', 'ASC']] }),
+    );
+    res.status(200).json(threads);
+  } catch (error) {
+    // console.error(error);
+    if (RtValidationError.guard(error)) {
+      if (error.code === 'CONTENT_INCORRECT' && error.details) {
+        res.status(400).json({ error: error.details });
+      } else {
+        res.status(500);
+      }
+    } else {
+      res.status(500);
+    }
+  }
+};
+
 export default {
   getTopicsController,
   getTopicByIdController,
+  getThreadsOfTopicController,
 };
