2d34fb9edd3315d9cd52ccea1ff01a65cb0494ac
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-10 18:03:39 -0500

    Refactor topic controllers with error handler

--- src/api/topic/topic.controllers.ts
+++ src/api/topic/topic.controllers.ts
@@ -1,48 +1,33 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Thread from '../thread/thread.model';
 import { ThreadArray } from '../thread/thread.types';
 import Topic from './topic.model';
 import {
-  RtValidationError,
   Topic as TopicType,
   TopicArray as TopicArrayType,
   RequestIdParam,
 } from './topic.types';
 
-const getTopicsController = async (_req: Request, res: Response) => {
+const getTopicsController = async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const topics = TopicArrayType.check(await Topic.findAll());
     res.json(topics);
   } catch (error: unknown) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-        return;
-      }
-    }
-
-    res.status(500);
+    next(error);
   }
 };
 
-const getTopicByIdController = async (req: Request, res: Response) => {
+const getTopicByIdController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = RequestIdParam.check({ id: req.params['id'] });
     const topic = TopicType.check(await Topic.findByPk(id));
     res.status(200).json(topic);
   } catch (error: unknown) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-        return;
-      }
-    }
-
-    res.status(500);
+    next(error);
   }
 };
 
-const getThreadsOfTopicController = async (req: Request, res: Response) => {
+const getThreadsOfTopicController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = RequestIdParam.check({ id: req.params['id'] });
     const threads = ThreadArray.check(
@@ -50,16 +35,7 @@ const getThreadsOfTopicController = async (req: Request, res: Response) => {
     );
     res.status(200).json(threads);
   } catch (error) {
-    // console.error(error);
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-      } else {
-        res.status(500);
-      }
-    } else {
-      res.status(500);
-    }
+    next(error);
   }
 };
 
