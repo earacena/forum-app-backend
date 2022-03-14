79772d9c54d889efb5ebd23c9bd5fcb1acd09502
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-13 20:20:18 -0400

    Refactor error handling for user controllers

--- src/api/user/user.controllers.ts
+++ src/api/user/user.controllers.ts
@@ -1,31 +1,23 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import {
   User as UserType,
   UserArray as UserArrayType,
   UserPostRequest,
-  RtValidationError,
 } from './user.types';
 import User from './user.model';
 import { RequestIdParam } from '../post/post.types';
 
-const getUsersController = async (_req: Request, res: Response) => {
+const getUsersController = async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const users = UserArrayType.check(await User.findAll());
     res.json(users);
   } catch (error) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-        return;
-      }
-    }
-
-    res.status(404).json({ error: 'users not found' });
+    next(error);
   }
 };
 
-const createUserController = async (req: Request, res: Response) => {
+const createUserController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, username, password } = UserPostRequest.check(req.body);
     const passwordHash = await bcrypt.hash(password, 10);
@@ -39,38 +31,21 @@ const createUserController = async (req: Request, res: Response) => {
 
     res.status(201).json(newUser);
   } catch (error) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-        return;
-      }
-    }
-
-    res.status(400).json({ error: 'invalid request' });
+    next(error);
   }
 };
 
-const getUserByIdController = async (req: Request, res: Response) => {
+const getUserByIdController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = RequestIdParam.check({ id: req.params['id'] });
     const user = UserType.check(await User.findByPk(id));
     res.status(200).json(user);
   } catch (error) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        res.status(400).json({ error: error.details });
-        return;
-      }
-
-      res.status(400).json({ error: 'invalid id given' });
-      return;
-    }
-
-    res.status(404).json({ error: 'user not found' });
+    next(error);
   }
 };
 
-const deleteUserByIdController = async (req: Request, res: Response) => {
+const deleteUserByIdController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = RequestIdParam.check({ id: req.params['id'] });
     await User.destroy({
@@ -79,19 +54,7 @@ const deleteUserByIdController = async (req: Request, res: Response) => {
 
     res.status(204).end();
   } catch (error) {
-    if (RtValidationError.guard(error)) {
-      if (error.code === 'CONTENT_INCORRECT' && error.details) {
-        if ('decodedToken' in error.details) {
-          res.status(401).json({ error: 'invalid or missing token' });
-          return;
-        }
-
-        res.status(400).json({ error: error.details });
-        return;
-      }
-    }
-
-    res.status(400).json({ error: 'invalid request' });
+    next(error);
   }
 };
 
