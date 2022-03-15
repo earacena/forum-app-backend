25ce37f8e2eb94d07edd2a67a5a1f89e2af88362
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-14 20:47:46 -0400

    Implement controller for creating topics

--- src/api/topic/topic.controllers.ts
+++ src/api/topic/topic.controllers.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import Role from '../role/role.model';
 import Thread from '../thread/thread.model';
 import { ThreadArray } from '../thread/thread.types';
 import Topic from './topic.model';
@@ -6,7 +7,12 @@ import {
   Topic as TopicType,
   TopicArray as TopicArrayType,
   RequestIdParam,
+  TopicPostRequest,
+  decodedToken as decodedTokenType,
 } from './topic.types';
+import { Role as RoleType } from '../role/role.types';
+import { User as UserType } from '../user/user.types';
+import User from '../user/user.model';
 
 const getTopicsController = async (_req: Request, res: Response, next: NextFunction) => {
   try {
@@ -39,8 +45,34 @@ const getThreadsOfTopicController = async (req: Request, res: Response, next: Ne
   }
 };
 
+const createTopicController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { title, description, decodedToken } = TopicPostRequest.check(req.body);
+    const token = decodedTokenType.check(decodedToken);
+    const user = UserType.check(await User.findByPk(token.id));
+
+    // User must be admin to create topics
+    const userRole = RoleType.check(Role.findByPk(user.id));
+    if (!userRole || userRole.role !== 'admin') {
+      res.status(401).json({ error: 'not authorized to create topics' }).end();
+      return;
+    }
+
+    const newTopic = await Topic.create({
+      userId: user.id,
+      title,
+      description,
+    });
+
+    res.status(201).json(newTopic);
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
 export default {
   getTopicsController,
   getTopicByIdController,
   getThreadsOfTopicController,
+  createTopicController,
 };
