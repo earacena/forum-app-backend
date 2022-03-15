3f0ba9164253b13a79a79f885c9299f97d0be908
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-14 21:10:11 -0400

    Add topic creation controller to topic routes

--- src/api/topic/topic.routes.ts
+++ src/api/topic/topic.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import auth from '../../middleware/authenticate';
 import topicControllers from './topic.controllers';
 
 const {
   getTopicsController,
   getTopicByIdController,
   getThreadsOfTopicController,
+  createTopicController,
 } = topicControllers;
 
 const topicsRouter = express.Router();
@@ -12,5 +14,6 @@ const topicsRouter = express.Router();
 topicsRouter.get('/', getTopicsController);
 topicsRouter.get('/:id', getTopicByIdController);
 topicsRouter.get('/:id/threads', getThreadsOfTopicController);
+topicsRouter.post('/', auth, createTopicController);
 
 export default topicsRouter;
