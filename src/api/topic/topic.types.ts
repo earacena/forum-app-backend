6f64de68904ccd1c7eb3ef3382ee3c464a860298
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-14 20:10:17 -0400

    Fix syntax

--- src/api/topic/topic.types.ts
+++ src/api/topic/topic.types.ts
@@ -18,11 +18,7 @@ export const Topic = RtRecord({
     RtInstanceOf(Date),
     RtString.withConstraint((x: string) => {
       // Must be parsable into  date string
-      if (!x
-        || x === null
-        || typeof x !== 'string'
-        || Number.isNaN(Date.parse(x))
-      ) {
+      if (!x || x === null || typeof x !== 'string' || Number.isNaN(Date.parse(x))) {
         return false;
       }
 
