1bd4fd8ff8fef382bd45cccc49e9a307bc2049f7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-20 22:28:03 -0400

    Implement controller that gets topics of forum

--- src/api/forum/forum.controllers.ts
+++ src/api/forum/forum.controllers.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
-import { decodedToken as decodedTokenType } from '../../common.types';
+import { decodedToken as decodedTokenType, RequestIdParam } from '../../common.types';
 import User from '../user/user.model';
 import Role from '../role/role.model';
 import { User as UserType } from '../user/user.types';
 import Forum from './forum.model';
 import { Forum as ForumType, ForumPostRequest } from './forum.types';
 import Topic from '../topic/topic.model';
+import { TopicArray } from '../topic/topic.types';
 
 const createForumController = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -44,6 +45,19 @@ const createForumController = async (req: Request, res: Response, next: NextFunc
   }
 };
 
+const getTopicsOfForumController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = RequestIdParam.check({ id: req.params['id'] });
+
+    // Fetch topics with givin forum id
+    const topics = TopicArray.check(await Topic.findAll({ where: { forumId: id } }));
+    res.status(200).json(topics);
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
 export default {
   createForumController,
+  getTopicsOfForumController,
 };
